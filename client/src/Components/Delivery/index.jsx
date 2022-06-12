111f84d394026ccf63c4d236134e9f5572c924c8
Harshal <harshrathod1111@gmail.com>
2022-06-13 02:56:38 +0530

    add food carousel to delivery page

---
+++ client/src/Components/Delivery/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//components
+import DeliveryCarousel from "./DeliveryCarousel";
+import RestaurantCard from "../RestaurantCard";
+
+const Delivery = () => {
+  return (
+    <>
+      <DeliveryCarousel />
+      <RestaurantCard />
+    </>
+  );
+};
+
+export default Delivery;
--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Delivery from "./Delivery/index";
 
 const Master = () => {
-    const {type} = useParams(); 
+  const { type } = useParams();
 
-  return (
-    <div>{type}</div>
-  )
-}
+  return <div className="my-5">{type === "delivery" && <Delivery />}</div>;
+};
 
 export default Master;
